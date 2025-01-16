a2e49f0feb28c1a9e89ed124f1cc43e336ad29a5
haruni24 <haruni20@icloud.com>
2025-01-16 10:24:01 +0900

    :sparkles: UCT-原始モンテカルロ法を実装

--- alphazero/MCTS.py
+++ alphazero/MCTS.py
@@ -3,21 +3,25 @@ from reversi.common import *
 
 class MCTS:
     def __init__(self):
-        pass
+        self.C = 1.0
 
     def search(self, board, num_playouts):
-        winning_rates = []
+        legal_move_num = len(board.legal_moves)
+        prob_list = []
+        uct_score, try_count, win_count = [1] * legal_move_num, [0] * legal_move_num, [0] * legal_move_num
         current_turn = board.turn
         board.remember()
-        for move in board.legal_moves:
-            win_count = 0
-            for _ in range(num_playouts):
-                board.push(move)
-                result = self.playout(board)
-                if result==current_turn: win_count+=1
-                board.pop()
-            winning_rates.append(win_count/num_playouts)
-        return board.legal_moves[np.argmax(winning_rates)]
+        for i in range(num_playouts):
+            move = board.legal_moves[i] if i<legal_move_num else np.random.choice(board.legal_moves, p=prob_list)
+            move_index = board.legal_moves.index(move)
+            try_count[move_index] += 1
+            board.push(move)
+            result = self.playout(board)
+            if result==current_turn: win_count[move_index] += 1
+            uct_score[move_index] = win_count[move_index]/try_count[move_index] + self.C * np.sqrt(np.log(i+1)/try_count[move_index])
+            prob_list = converse_prob(uct_score)
+            board.pop()
+        return board.legal_moves[np.argmax(try_count)]
 
     def playout(self, board):
         while not board.end:
--- reversi/common.py
+++ reversi/common.py
@@ -15,3 +15,8 @@ def move_to_pos(x, y):
     col = string.ascii_uppercase[x]
     row = y + 1
     return f"{col}{row}"
+
+def converse_prob(element_list):
+    element_sum = sum(element_list)
+    prob = [element/element_sum for element in element_list]
+    return prob
